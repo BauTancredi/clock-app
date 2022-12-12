74f42635895001789caf524eadab7dceb75dcbd8
BauTancredi <bautistatancredi@hotmail.com>
2022-12-12 14:18:33 +1100

    Added basic animation and behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import React, { useRef } from "react";
+
 function App() {
+  const ref = useRef(null);
+
+  const toggle = () => {
+    ref.current.classList.toggle("down");
+    ref.current.classList.toggle("up");
+  };
+
   return (
-    // <div className="flex">
-    <h1 className="text-center underline">hello world</h1>
-    // </div>
+    <div ref={ref} className="down">
+      <div className="h-screen bg-transparent w-full first flex items-between">
+        <h1>Dissapear</h1>
+        <button onClick={toggle}> More</button>
+      </div>
+      <div className="h-80 bg-slate-600 second">
+        <h1>Appear</h1>
+      </div>
+    </div>
   );
 }
 
